89e7a89606978db3af4ae7b3bfd27e313e9d4008
John Fisher <jkfisher97@comcast.net>
2019-07-27 11:26:18 -0600

    Added proirity queue

--- LinkedLists/Node.cs
+++ LinkedLists/Node.cs
@@ -7,21 +7,45 @@ namespace DataStructures
 
         public Node Previous {get;set;}
 
+        public int Priority {get; set;}
+
         public Node(int value){
             Value = value;
             Next = null;
             Previous = null;
         }
+         public Node(int value, int priority){
+            Value = value;
+            Next = null;
+            Previous = null;
+            Priority = priority;
+        }
 
         public Node(int value, Node next){
             Value = value;
             Next = next;
+            Priority = 0;
+        }
+        public Node(int value, Node next, int priority){
+            Value = value;
+            Next = next;
+            Priority = priority;
         }
 
         public Node(int value, Node next, Node previous){
             Value = value;
             Next = next;
             Previous = previous;
+            Priority = 0;
         }
+
+           public Node(int value, Node next, Node previous, int priority){
+            Value = value;
+            Next = next;
+            Previous = previous;
+            Priority = priority;
+        }
+
+   
     }
 }
---
+++ Queues/PriorityQueue.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using DataStructures;
+namespace Queue.Priority
+{
+        /*Examples from Pluralsight course "Algorithms and Data Structures - Part 1 by Robert Horvick*/
+
+        /// <summary>
+        /// A collection that returns the highest priority item first and lowest priority item last
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class PriorityQueue<Node> : IEnumerable<DataStructures.Node>{
+
+            //allocate the list
+            private LinkedList<DataStructures.Node> _list = new LinkedList<DataStructures.Node>();
+        
+            /// <summary>
+            /// Add the item  to the queue in priority order
+            /// </summary>
+            /// <param name="item"></param>
+            public void Enqueue(DataStructures.Node item){
+                if(_list.Count == 0){
+                    _list.AddLast(item);
+
+                }else{
+                    //find the proper insert point
+                    var current = _list.First;
+
+                    //while we're not at the end of the list and current value
+                    //is larger than the value being inserted...
+                    while(current != null && CompareTo(current.Value, item) > 0){
+                        current = current.Next;
+                    }
+                    if(current == null){
+                        //we made it to the end of the list
+                        _list.AddLast(item);
+                    }else{
+                        //the current item is <= to the one being added
+                        //so add the item before it
+                        _list.AddBefore(current, item);
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Removes and returns the highest priority item from the queue
+            /// </summary>
+            /// <returns></returns>
+             public DataStructures.Node Dequeue(){
+                if(_list.Count == 0){
+                    throw new InvalidOperationException("The queue is empty");
+                }
+
+                //store the laSt value in the temp variable
+                DataStructures.Node value = _list.First.Value;
+
+                //remove the last item
+                _list.RemoveFirst();
+
+                //return the stored last value
+                return value;
+            }
+
+
+            /// <summary>
+            /// Returns the top item from the queue without removing it from the queue
+            /// </summary>
+            /// <returns></returns>
+             public DataStructures.Node Peek(){
+                if(_list.Count == 0){
+                    throw new InvalidOperationException("The Stack is empty");
+                }
+                return _list.First.Value;
+             
+            }
+
+            /// <summary>
+            /// The current number of items in the queue
+            /// </summary>
+            /// <returns></returns>
+            public int Count {
+                get {
+                        return _list.Count;
+                }
+             
+            }
+
+            /// <summary>
+            /// Removes all items from the queue
+            /// </summary>
+            public void Clear(){
+                _list.Clear();
+            }
+
+            /// <summary>
+            /// Returns an enumerator that enumerates the queue
+            /// </summary>
+            /// <returns></returns>
+            public IEnumerator<DataStructures.Node> GetEnumerator(){
+                return _list.GetEnumerator();
+            }
+
+
+            /// <summary>
+            /// Returns an enumerator that enumerates the queue
+            /// </summary>
+            /// <returns></returns>
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator(){
+                return _list.GetEnumerator();
+            }
+
+            // Compares by Length, Height, and W<idth.
+
+        public int CompareTo(DataStructures.Node a, DataStructures.Node b){
+            if(a.Value > b.Value){
+                return 1;
+            }else if (a.Value < b.Value){
+                return -1;
+            }else{
+                return 0;
+            }
+            
+        }
+
+
+
+        
+        }
+    
+}
